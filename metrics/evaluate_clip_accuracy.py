dde849701547d0d9e717051af371f669095a8298
miloszbargiel <miloszbargiel@gmail.com>
2025-11-03 11:18:52 +0100

    Refactor evaluate_clip_accuracy script
    (cherry picked from commit f9491e78af535c04f5855b9a7369a123ce131861)

--- metrics/evaluate_clip_accuracy.py
+++ metrics/evaluate_clip_accuracy.py
@@ -10,7 +10,7 @@ import json
 
 
 @torch.no_grad()
-def calculate_mean_prob(image_dir, object_ls, save_path):
+def calculate_mean_prob(image_dir, object_ls, save_path, save_means):
     '''
     Returns a dataframe, where the first column is the image name, and the next ten columns are the clip_score with each object
     ------------------------------
@@ -45,48 +45,58 @@ def calculate_mean_prob(image_dir, object_ls, save_path):
 
     prob_df = pd.DataFrame(prob_results)
 
-    if save_path is not None:
+    if save_path:
         prob_df.to_csv(f'{save_path}/{image_dir.replace("/","_")}.csv')
-    if save_path is None:
-        print('do not need to save detailed probability results')  
+    else:
+        print('do not need to save detailed probability results')
     
     caring_object= image_dir.split('/')[-1]
     if "_paraphrases" in caring_object:
         caring_object = caring_object.replace("_paraphrases", "")
 
-    caring_average_prob=prob_df[caring_object].mean()
+    if save_means:
+        prob_df.to_csv(f'{save_path}/{caring_object}.csv')
+        caring_average_prob=prob_df[caring_object].mean()
     
-    return(caring_average_prob)
+        return caring_average_prob
+    return 0.0
 
 
 if __name__=='__main__':
     parser = ArgumentParser()
     parser.add_argument("--base_folder", type=str, required=True)
-    parser.add_argument("--save_path", type=str, required=False, default=None)  
+    parser.add_argument("--n_images_in_dir", type=int, required=True)
+    parser.add_argument("--save_path", type=str, required=True)
+    parser.add_argument("--save_means", type=bool, required=False, default=False)
     args = parser.parse_args()
 
     base_folder=args.base_folder
     save_path=args.save_path
+    save_means=args.save_means
 
     with open('./prompts_csv/10_objects_paraphrase.json','r') as file:
         object_dic=json.load(file)
     object_ls=list(object_dic.keys())
 
-    def process_folder_recursive(base_folder):
+    def process_folder_recursive(base_folder, n_files_in_dir):
         results = {}
-        if len(os.listdir(base_folder)) > 100:        # end folder
+        if len(os.listdir(base_folder)) == n_files_in_dir:        # end folder
             print('End folder is:', base_folder)
-            result = calculate_mean_prob(base_folder, object_ls, save_path)
+            result = calculate_mean_prob(base_folder, object_ls, save_path, save_means)
             results[base_folder] = result
         else:                                         # parent folder
             for folder_name in os.listdir(base_folder):
                 folder_path = os.path.join(base_folder, folder_name)
-                results.update(process_folder_recursive(folder_path))  
+                results.update(process_folder_recursive(folder_path, n_files_in_dir))
         return results
 
-    all_results=process_folder_recursive(base_folder)
-    
-    print('==========================================')
-    for folder_path, result in all_results.items():
-        print(f"{folder_path}: {result}")
+    all_results=process_folder_recursive(base_folder, args.n_images_in_dir)
+    if save_means:
+        all_results = {key.split("/")[-1]: str(value) for key, value in all_results.items()}
+        all_results = [",".join(item) for item in all_results.items()]
+        headers = ["category", "accuracy"]
+        with open(f"{save_path}/all_results.csv","w") as file:
+            file.write(",".join(headers))
+            file.write("\n")
+            file.write("\n".join(all_results))
 
